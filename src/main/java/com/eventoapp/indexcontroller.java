333ce0061806aadc9f0267b976f6555ad70fa9aa
PhilipeGama <philipegama97@gmail.com>
2021-08-11 23:17:25 -0400

    ajeitando get /

--- src/main/java/com/eventoapp/indexcontroller.java
+++ src/main/java/com/eventoapp/indexcontroller.java
@@ -1,12 +1,23 @@
 package com.eventoapp;
 
+import com.eventoapp.models.Evento;
+import com.eventoapp.repository.EventoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class indexcontroller {
+    @Autowired
+    private EventoRepository er;
     @RequestMapping("/")
-    public String index(){
-        return "index";
+    public ModelAndView listarEventos(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventos = er.findAll();
+        mv.addObject("eventos", eventos);
+        
+        return mv;
     }
 }
